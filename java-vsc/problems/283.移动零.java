8070b6b76f0ec266805f730c6c525bfd3a57c415
liyuan <liyuan@agree.com.cn>
2020-04-21 09:25:01 +0800

    283.移动零

---
+++ java-vsc/problems/283.移动零.java
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode.cn id=283 lang=java
+ *
+ * [283] 移动零
+ */
+
+// @lc code=start
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int len = nums.length;
+        int zeroCount = 0;
+        for (int j = 0; j < len; j++) {
+            if (nums[j] != 0) {
+                int tmp = nums[j - zeroCount];
+                nums[j - zeroCount] = nums[j];
+                nums[j] = tmp;
+            } else {
+                zeroCount++;
+            }
+        }
+    }
+}
+// @lc code=end
+
+
+
+//class Solution {
+//     public void moveZeroes(int[] nums) {
+//         int len = nums.length;
+//         for (int i = 0, j = 0; j < len; j++) {
+//             if (nums[j] != 0) {
+//                 nums[i] = nums[j];
+//                 if (j != i) {
+//                     nums[j] = 0;
+//                 }
+//                 i++;
+//             }
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+// class Solution {
+//     public void moveZeroes(int[] nums) {
+//         int len = nums.length;
+//         int zeroCount = 0;
+//         for (int j = 0; j < len; j++) {
+//             if (nums[j] != 0) {
+//                 int tmp = nums[j - zeroCount];
+//                 nums[j - zeroCount] = nums[j];
+//                 nums[j] = tmp;
+//             } else {
+//                 zeroCount++;
+//             }
+//         }
+//     }
+// }
