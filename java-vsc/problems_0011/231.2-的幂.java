4308e9788f83f19ea822a5d2ee8314ba7871d2cd
ly21st <liyuan21st@foxmail.com>
2020-07-22 21:58:06 +0800

    2的幂

--- java-vsc/problems_0011/231.2-的幂.java
+++ java-vsc/problems_0011/231.2-的幂.java
@@ -45,3 +45,13 @@ class Solution {
 }
 // @lc code=end
 
+
+// 解法2
+// class Solution {
+//     public boolean isPowerOfTwo(int n) {
+//       if (n == 0) return false;
+//       while (n % 2 == 0) n /= 2;
+//       return n == 1;
+//     }
+//   }
+  
