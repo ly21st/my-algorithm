f67ae90aaa8fbfd7bcae56d9ceeb14cd35f278b7
ly21st <liyuan21st@foxmail.com>
2020-10-13 00:14:48 +0800

    树的遍历

---
+++ java-vsc/week-02/589.n叉树的前序遍历.java
@@ -0,0 +1,89 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import org.w3c.dom.Node;
+
+/*
+ * @lc app=leetcode.cn id=589 lang=java
+ *
+ * [589] N叉树的前序遍历
+ *
+ * https://leetcode-cn.com/problems/n-ary-tree-preorder-traversal/description/
+ *
+ * algorithms
+ * Easy (74.10%)
+ * Likes:    108
+ * Dislikes: 0
+ * Total Accepted:    49.1K
+ * Total Submissions: 66.2K
+ * Testcase Example:  '[1,null,3,2,4,null,5,6]'
+ *
+ * 给定一个 N 叉树，返回其节点值的前序遍历。
+ * 
+ * 例如，给定一个 3叉树 :
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 返回其前序遍历: [1,3,5,6,2,4]。
+ * 
+ * 
+ * 
+ * 说明: 递归法很简单，你可以使用迭代法完成此题吗?
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<Integer> preorder(Node root) {
+        List<Integer> res = new ArrayList<>();
+        helper(root, res);
+        return res;
+    }
+    public void helper(Node root, List<Integer> res) {
+        if (root == null) return;
+        res.add(root.val);
+        for (Node node : root.children) {
+            helper(node, res);
+        }
+    }
+}
+// @lc code=end
+
+
+// 递归法
+class Solution {
+    public List<Integer> preorder(Node root) {
+        List<Integer> res = new ArrayList<>();
+        helper(root, res);
+        return res;
+    }
+    public void helper(Node root, List<Integer> res) {
+        if (root == null) return;
+        res.add(root.val);
+        for (Node node : root.children) {
+            helper(node, res);
+        }
+    }
+}
